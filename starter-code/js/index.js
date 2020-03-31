5eb5913c31a210a447994db4f4a654d75f2120bd
emelko <ml.kochsiek@mailbox.org>
2020-03-31 12:02:53 +0200

    Next iteration, incomplete; @alba-tros

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,10 +2,27 @@ var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
 function updateSubtotal($product) {
-  // Iteration 1.1
+  let prices = [];
+  let quantity = [];
+  let subtotal = [];
+  document.querySelectorAll('.pu span').forEach(onePrice => {
+    console.log(onePrice.innerHTML)
+    prices.push(parseFloat(onePrice.innerHTML))
+  })
+  document.querySelectorAll('.qty input').forEach(q => {
+    console.log(q.value)
+    quantity.push(parseInt(q.value))
+  })
+  document.querySelectorAll('.subtot span').forEach(subt => {
+    subtotal.push(subt.innerHTML)
+    for (let i = 0; i < prices.length; i++) {
+      console.log(subtotal[i] = prices[i] * quantity[i])
+    }
+  })
 }
 
+
 function calculateAll() {
-  // Iteration 1.2
+  updateSubtotal()
 }
 $calc.onclick = calculateAll;
